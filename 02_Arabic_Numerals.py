fa7994067d52c80b2547d534c4a48e8393b4eaca
Tanakrit Rangchankul <76813863+Skizzdag@users.noreply.github.com>
2024-03-20 16:31:09 +0700

    Delete 02_Arabic_Numerals.py

--- 02_Arabic_Numerals.py
+++
@@ -1,5 +0,0 @@
-def number_name(n):
-  n = int(n)
-  number = ["zero","one","two","three","four","five","six","seven","eight","nine"]
-  return print(str(n)+" --> "+str(number[n]))
-number_name(input())
