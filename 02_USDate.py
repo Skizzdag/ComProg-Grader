903b7da2fe1a1634c3fc2f434f6d63ca46d9fc97
Tanakrit Rangchankul <76813863+Skizzdag@users.noreply.github.com>
2024-03-20 16:32:21 +0700

    Delete 02_USDate.py

--- 02_USDate.py
+++
@@ -1,5 +0,0 @@
-def US_date(a):
-    month = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-    return print(month[int(a[1])-1] +" ", a[0] + ", " + a[2])
-inp = input("").split("/")
-US_date(inp)
